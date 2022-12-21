4b476c75a74f6a9562c99de6895679694c94a4f9
mammadovrashad <talehg2932@gmail.com>
2022-12-21 13:47:10 +0400

    finsih

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Form from './components/form/Form'
+import Header from './components/header/header'
+import List from './components/List/List'
+import './App.css'
+import { useState } from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import ListLink from './components/listLink/ListLink'
+const App = () => {
+  const [list,setList]=useState([])
+  return (
+    <BrowserRouter>
+      <Header/>
+      <div className='row'>
+        <Routes>
+          <Route path='/list' element={<ListLink/>} ></Route>
+        </Routes>
+        <Form setList={setList} list={list} />
+        <List list={list} setList={setList}/>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
